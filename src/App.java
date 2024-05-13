372ff3c4e3f17545a30c9566297ef8999f51d1ac
Eliseo Isaí Serrano Guzmán <sg24002@esfe.agape.edu.sv>
2024-05-13 11:28:54 -0600

    se añadieron variables

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        float horasTrabajadas = 0;
+        float pagoXHora = 0;
+        float pagoTotal = 0;
     }
 }
