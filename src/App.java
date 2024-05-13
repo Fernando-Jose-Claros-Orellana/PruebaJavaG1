466527ab76682cb1cdce5abbfe1de60c7c7f3649
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-05-13 11:31:34 -0600

    Se agrego el scanner

--- src/App.java
+++ src/App.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner pedir = new Scanner(System.in);
+
         float horasTrabajadas = 0;
         float pagoXHora = 0;
         float pagoTotal = 0;
+
+        
+
+
     }
 }
